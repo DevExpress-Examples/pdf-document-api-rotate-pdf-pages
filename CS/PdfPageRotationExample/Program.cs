346691a1f98622e7e67e8f8741fd3ef480acd303
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PdfPageRotationExample/Program.cs
@@ -0,0 +1,21 @@
+﻿#region #Reference
+using DevExpress.Pdf;
+// ...
+#endregion #Reference
+
+namespace PdfPageRotationExample {
+#region #Code
+class Program {
+    static void Main(string[] args) {
+        PdfDocumentProcessor pdfDocumentProcessor = new PdfDocumentProcessor();
+        pdfDocumentProcessor.LoadDocument("..\\..\\docs\\TextRotate.pdf");
+        int angle = 0;
+        foreach (PdfPageTreeObject page in pdfDocumentProcessor.Document.Pages) {
+            angle = (angle + 90) % 360;
+            page.Rotate = angle;
+        }
+        pdfDocumentProcessor.SaveDocument("..\\..\\docs\\Rotated.pdf");
+    }
+}
+#endregion #Code
+}
---
+++ VB/PdfPageRotationExample/Program.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+#Region "#Reference"
+Imports DevExpress.Pdf
+' ...
+#End Region ' #Reference
+
+Namespace PdfPageRotationExample
+#Region "#Code"
+Friend Class Program
+	Shared Sub Main(ByVal args() As String)
+		Dim pdfDocumentProcessor As New PdfDocumentProcessor()
+		pdfDocumentProcessor.LoadDocument("..\..\docs\TextRotate.pdf")
+		Dim angle As Integer = 0
+		For Each page As PdfPageTreeObject In pdfDocumentProcessor.Document.Pages
+			angle = (angle + 90) Mod 360
+			page.Rotate = angle
+		Next page
+		pdfDocumentProcessor.SaveDocument("..\..\docs\Rotated.pdf")
+	End Sub
+End Class
+#End Region ' #Code
+End Namespace
