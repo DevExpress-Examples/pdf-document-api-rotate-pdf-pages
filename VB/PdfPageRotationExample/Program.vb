3bc4365cbd9b8d5261693b80b9e77a7a8dc10163
DevExpressExampleBot <fakeEmail>
2022-01-28 14:11:42 +0300

    Source auto update [skip ci]

--- VB/PdfPageRotationExample/Program.vb
+++ VB/PdfPageRotationExample/Program.vb
@@ -1,23 +1,24 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-#Region "#Reference"
+'#Region "#Reference"
 Imports DevExpress.Pdf
-' ...
-#End Region ' #Reference
 
+' ...
+'#End Region  ' #Reference
 Namespace PdfPageRotationExample
-#Region "#Code"
-Friend Class Program
-	Shared Sub Main(ByVal args() As String)
-		Dim pdfDocumentProcessor As New PdfDocumentProcessor()
-		pdfDocumentProcessor.LoadDocument("..\..\docs\TextRotate.pdf")
-		Dim angle As Integer = 0
-		For Each page As PdfPageTreeObject In pdfDocumentProcessor.Document.Pages
-			angle = (angle + 90) Mod 360
-			page.Rotate = angle
-		Next page
-		pdfDocumentProcessor.SaveDocument("..\..\docs\Rotated.pdf")
-	End Sub
-End Class
-#End Region ' #Code
+
+'#Region "#Code"
+    Friend Class Program
+
+        Shared Sub Main(ByVal args As String())
+            Dim pdfDocumentProcessor As PdfDocumentProcessor = New PdfDocumentProcessor()
+            pdfDocumentProcessor.LoadDocument("..\..\docs\TextRotate.pdf")
+            Dim angle As Integer = 0
+            For Each page As PdfPageTreeObject In pdfDocumentProcessor.Document.Pages
+                angle =(angle + 90) Mod 360
+                page.Rotate = angle
+            Next
+
+            pdfDocumentProcessor.SaveDocument("..\..\docs\Rotated.pdf")
+        End Sub
+    End Class
+'#End Region  ' #Code
 End Namespace
